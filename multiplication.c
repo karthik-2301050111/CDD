9d80ad15cce976877cdc27e40e94d8232760936d
karthik-2301050111 <2301050111@kluniversity.in>
2024-02-08 15:42:22 +0530

    the 3 files are for the addition,substraction,multiplication

---
+++ multiplication.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	int e,f,mul;
+	printf("Enter the values for multiplication");
+	scanf("%d %d,&e,&f);
+	mul = e*f;
+	printf("%d",mul);
+	return 0;
+}
---
+++ subtraction.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+	int c,d,sub=0;
+	printf("Enter the values of the a and b");
+	scanf("%d %d,&c,&d);
+	sub = c-d;
+	printf("%d",&sub);
+	return 0;
+}
